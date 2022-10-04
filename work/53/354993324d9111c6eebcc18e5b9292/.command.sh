0a60e853353cbf6e0203c28e51cc05235de74047
jaydee <68099601+asereewit@users.noreply.github.com>
2022-10-04 16:03:38 -0700

    add minimum median coverage of 3 for consensus calling

--- work/53/354993324d9111c6eebcc18e5b9292/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-blastn -out example_KF923898.1_HCOV_blast_output.txt -query example_KF923898.1_HCOV.consensus_final.fa -db VP1_164_annotated_nospaces.fasta -outfmt 6 -task blastn -max_target_seqs 1 -evalue 1e-5
-# outfmt6 default values: 'qaccver saccver pident length mismatch gapopen qstart qend sstart send' 
-
-serotype=$(awk 'FNR==1{print val,$2}' example_KF923898.1_HCOV_blast_output.txt | cut -d "_" -f2- | cut -d "/" -f2-)
-echo $serotype > example_KF923898.1_HCOV_serotype.txt
--- work/f7/ca9aa58831f512384f165a85c83a90/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-blastn -out example_KY369892.1_RV_blast_output.txt -query example_KY369892.1_RV.consensus_final.fa -db VP1_164_annotated_nospaces.fasta -outfmt 6 -task blastn -max_target_seqs 1 -evalue 1e-5
-# outfmt6 default values: 'qaccver saccver pident length mismatch gapopen qstart qend sstart send' 
-
-serotype=$(awk 'FNR==1{print val,$2}' example_KY369892.1_RV_blast_output.txt | cut -d "_" -f2- | cut -d "/" -f2-)
-echo $serotype > example_KY369892.1_RV_serotype.txt
